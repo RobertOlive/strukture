d6cf5bdaa45ef2f0c217cbd9979bb24a40595d7b
Robert Olive <robert.aaron.olive@gmail.com>
2018-07-31 15:18:03 -0400

    minor formatting

--- client/src/components/TodoItem.js
+++ client/src/components/TodoItem.js
@@ -7,12 +7,13 @@ class TodoItem extends Component {
                 year = time.getFullYear(),
                 month = time.getMonth(),
                 day = time.getDate(),
-                hour = time.getHours(),
-                minute = time.getMinutes() < 10? "0" + time.getMinutes() : time.getMinutes();
+                hour = time.getHours() > 12? time.getHours() - 12 : time.getHours(),
+                minute = time.getMinutes() < 10? "0" + time.getMinutes() : time.getMinutes(),
+                ampm = time.getHours() > 12? "p.m" : "a.m.";
         return (
             <li className="list-group-item">
                 {(this.props.userData.user? 
-                    (<div><span className="timeSpan"><h3>{hour + ":" + minute + " "}<br></br></h3></span>
+                    (<div><span className="timeSpan"><h3>{hour + ":" + minute + " " + ampm}<br></br></h3></span>
                         <span className="taskMove"><strong>Task: </strong>{this.props.item.task} <br></br></span>
                         <span className="taskMove"><strong>Cost: </strong>${this.props.item.cost} <br></br></span>
                         <span className="taskMove"><strong>Location: </strong>{this.props.item.location} <br></br></span>
